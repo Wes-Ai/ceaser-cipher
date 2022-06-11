c0aae5693011b295b6b1f4c47c60161349d78012
Wesley <smithw26@wwu.edu>
2022-06-10 21:03:00 -0700

    Working out filter.map logic; might go with another method.

--- cipher.rb
+++ cipher.rb
@@ -1,11 +1,11 @@
 def caeser_cipher(message, shift_factor)
     seperated_message = message.split('')
-    p shift_char(seperated_message, shift_factor)
+    return p shift_char(seperated_message, shift_factor)
 
 end
 
 def shift_char(char_array, shift_factor)
-    char_array.map { |char| (char.ord + shift_factor).chr }
+    char_array.filter_map { |char| (char.ord + shift_factor).chr if char=~/[[:alpha:]]/ }
 end
 
 caeser_cipher("Who are you?", 2)

27fefae89fd2e3f7a85aa0ec4a8ef21ed3d72d8c
Wesley <smithw26@wwu.edu>
2022-06-10 20:57:14 -0700

    Shift_char returns array, but does not ignore spaces and special chars.

--- cipher.rb
+++ cipher.rb
@@ -1,10 +1,11 @@
 def caeser_cipher(message, shift_factor)
     seperated_message = message.split('')
-    p seperated_message[0].ord
+    p shift_char(seperated_message, shift_factor)
+
 end
 
 def shift_char(char_array, shift_factor)
-    char_array.each do |char| char = (char.ord + shift_factor).chr
+    char_array.map { |char| (char.ord + shift_factor).chr }
 end
 
 caeser_cipher("Who are you?", 2)

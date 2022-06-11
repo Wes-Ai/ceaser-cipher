d957b8a19f527391cf04d5986dc16f2d2d7d0667
Wesley <smithw26@wwu.edu>
2022-06-11 09:54:51 -0700

    Complete refactor; currently prints string of nums with spaces preserved.

--- cipher.rb
+++ cipher.rb
@@ -1,11 +1,18 @@
+def letter?(lookAhead)
+    lookAhead.match?(/[[:alpha:]]/)
+end
+
 def caeser_cipher(message, shift_factor)
-    seperated_message = message.split('')
-    return p shift_char(seperated_message, shift_factor)
-
+    seperated_numbers = (message.split('')).map do |char| 
+        if(letter?(char))
+            char.ord
+        else
+            char
+        end
+    end
 end
 
-def shift_char(char_array, shift_factor)
-    char_array.filter_map { |char| (char.ord + shift_factor).chr if char=~/[[:alpha:]]/ }
-end
 
-caeser_cipher("Who are you?", 2)
+
+
+p caeser_cipher("Who are you?", 2)

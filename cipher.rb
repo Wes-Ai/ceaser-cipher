0d4d33eb740cf1f563ed0c4d5115249efa901238
Wesley <smithw26@wwu.edu>
2022-06-10 20:51:25 -0700

    Added shift_char method; testing logic.

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,10 @@
 def caeser_cipher(message, shift_factor)
     seperated_message = message.split('')
-    p seperated_message
+    p seperated_message[0].ord
+end
+
+def shift_char(char_array, shift_factor)
+    char_array.each do |char| char = (char.ord + shift_factor).chr
 end
 
 caeser_cipher("Who are you?", 2)

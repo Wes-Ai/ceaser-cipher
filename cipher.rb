99762d014c33f60848e65ba3b316b6a453879f34
Wesley <smithw26@wwu.edu>
2022-06-11 10:39:35 -0700

    Caeser cipher implemented, with some error cases.

--- cipher.rb
+++ cipher.rb
@@ -4,16 +4,38 @@ def letter?(lookAhead)
 end
 
 def caeser_cipher(message, shift_factor)
+    if(shift_factor > 10)
+        return "Shift factor cannot be above 9."
+    elsif(shift_factor < 0)
+        return "Shift factor cannot be 0 or below."
+    end
     seperated_numbers = (message.split('')).map do |char| 
         if(letter?(char))
-            char.ord
+            char_number = char.ord
+            char_number = char_number + shift_factor
+
+            if(char_number > 122)
+                char_number -= 26
+            elsif(char_number > 90 && char_number < 97)
+                char_number -= 26
+            else
+                char_number
+            end
+
+            char_number.chr
         else
             char
         end
     end
+
+    seperated_numbers.join('')
 end
 
 
 
 
-p caeser_cipher("Who are you?", 2)
+puts caeser_cipher("Who are youz?", 2)
+puts caeser_cipher("ABC DEF GHI JKL MNO PQR STU VWX YZ", 3)
+puts caeser_cipher("Wtf my man?", 9)
+puts caeser_cipher("Wtf my man?", 15)
+puts caeser_cipher("Wtf my man?", -5)
